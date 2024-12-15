f1f1b38819af0c0fe513245ce334a7b9faf19817
Kozhemaykin <kozhemaykin.dima@gmail.com>
2024-12-15 21:20:10 +0200

    new line

--- docker/mysql/run.sh
+++ docker/mysql/run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 # Создание директории для сокета
-if [ ! -d /var/run/mysqld ]; then
-    echo "[i] Создаём директорию для сокета..."
-    mkdir -p /var/run/mysqld
+if [ -d /var/run/mysqld ]; then
+    echo "[i] gift right directory ..."
     chown -R mysql:mysql /var/run/mysqld
+else
+    echo "[i] mysqld not found, creating...."
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
 fi
 
 # Удаление старого сокета
@@ -15,47 +18,52 @@ fi
 
 # Проверка переменных окружения
 if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-    echo "[!] Ошибка: MYSQL_ROOT_PASSWORD не задан!"
-    exit 1
+    MYSQL_ROOT_PASSWORD=$(pwgen 16 1)
+    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
 fi
 
 # Инициализация базы данных
-if [ ! -d /var/lib/mysql ]; then
+if [ -d /var/lib/mysql ]; then
+    echo "[i] MySQL directory already present, skipping creation"
+    chown -R mysql:mysql /var/lib/mysql
+else
+
+    echo "[i] MySQL data directory not found, creating initial DBs"
+    chown -R mysql:mysql /var/lib/mysql
+
     echo "[i] Инициализация MariaDB..."
-    mariadb-install-db --user=root --datadir=/var/lib/mysql > /dev/null
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
     echo "[i] Создаём начальную конфигурацию..."
 
     tfile=$(mktemp)
     cat << EOF > $tfile
 USE mysql;
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-FLUSH PRIVILEGES;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES
 EOF
-
-
-    mariadbd --user=root --bootstrap --password="$MYSQL_ROOT_PASSWORD" --verbose=0 < $tfile
-    rm -f $tfile
-
 
     if [ -n "$MYSQL_DATABASE" ]; then
         echo "[i] Создаём базу данных ..."
-        echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" | mariadb --user=root --password="$MYSQL_ROOT_PASSWORD"
+        echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
     fi
 
     if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ]; then
         echo "[i] Создаём кастомного пользователя и наделяем его правами ..."
-        echo "CREATE USER '$MYSQL_USER'@'$MYSQL_USER_HOST' IDENTIFIED BY '$MYSQL_PASSWORD';" | mariadb --user=root --password="$MYSQL_ROOT_PASSWORD"
-        echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'$MYSQL_USER_HOST';" | mariadb --user=root --password="$MYSQL_ROOT_PASSWORD"
-        echo "FLUSH PRIVILEGES;" | mariadb --user=root --password="$MYSQL_ROOT_PASSWORD"
+        echo "CREATE USER '$MYSQL_USER'@'$MYSQL_USER_HOST' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+        echo "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'$MYSQL_USER_HOST';" >> $tfile
+        echo "FLUSH PRIVILEGES;" >> $tfile
     fi
-
+    
+    mariadbd --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
+    rm -f $tfile
 fi
 
 # Запуск MariaDB
 echo "[i] Запуск MariaDB..."
-exec mariadbd-safe --console --datadir=/var/lib/mysql --user=root "$@"
+exec mariadbd --user=mysql --console --skip-name-resolve --skip-networking=0 $@
 
